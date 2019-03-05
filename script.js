a275c3f4d4b15ba1b81e4f53b15634d6855c07e4
yera05 <43611249+yera05@users.noreply.github.com>
2019-03-05 09:42:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+ $("button").click(function(){
+   var name = $(".name-input").val();
+   var eat = $(".eat-food").val();
+   var workout = $(".workout-excersice").val();
+   console.log(name);
+   console.log(eat);
+   console.log(workout); 
+});
